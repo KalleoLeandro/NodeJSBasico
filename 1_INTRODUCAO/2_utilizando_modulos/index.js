72e7799a0ab02ab9029bd88fdb7382b92d08f8ec
Kalleo Leandro <kalleo.leal@gmail.com>
2023-02-23 14:36:33 -0300

    Introducao

---
+++ 1_INTRODUCAO/2_utilizando_modulos/index.js
@@ -0,0 +1,9 @@
+// importando modulos
+const fs = require('fs'); //
+
+fs.readFile('arquivo.txt', 'utf-8', (err,data) =>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+});
